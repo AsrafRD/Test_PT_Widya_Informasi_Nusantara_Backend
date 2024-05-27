5bca887f598ccffaddf6e42d8f2f60c08fdfb4b4
AsrafRD <asrafraffi2019@gmail.com>
2024-05-27 18:29:42 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import cors from "cors";
-import authRoutes from './routes/authRoutes.js';
-import productRoutes from './routes/productRoutes.js';
+import argon2 from 'argon2';
+import jwt from 'jsonwebtoken';
+import User from './models/user.js';
 
 dotenv.config();
 
-connectDB();
-
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors({
-  credentials: true,
-  origin: 'https://my-auth-app-three.vercel.app', 
-}));
-
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-app.use('/auth', authRoutes);
-app.use('/products', productRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
+
+app.post('/register', async (req, res) => {
+  const { name, email, password, gender } = req.body;
+  try {
+    const hashedPassword = await argon2.hash(password);
+    const newUser = await User.create({ name, email, password: hashedPassword, gender });
+    res.status(201).json({ message: 'User created successfully', newUser });
+  } catch (error) {
+    res.status(400).json({ error: 'User already exists' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !await argon2.verify(user.password, password)) {
+      return res.status(400).json({ error: 'Account Not Found' });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+      console.log('MongoDB connected...');
+    });
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
