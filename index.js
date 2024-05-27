dcbd3cb7c1bf025cb62c40acdf9e593f58d43d88
AsrafRD <asrafraffi2019@gmail.com>
2024-05-27 20:00:22 +0700

    final

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ import cors from "cors";
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors({
   credentials: true,
